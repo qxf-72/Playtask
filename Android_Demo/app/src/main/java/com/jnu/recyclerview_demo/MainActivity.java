a6f00159ef5489cf06d0f14d7022a67cf948c554
XiaofengQiu <18320651165@163.com>
2023-10-12 09:57:42 +0800

    Update Tips:之前仓库存放到的是实验报告,在已经提交仓库链接给老师的情况下,才得知仓库存放的是项目代码,做了以下修改:     1.将仓库中的实验报告删除     2.将之前的实验项目copy到本仓库中,之后的项目修改将在本仓库中进行

---
+++ Android_Demo/app/src/main/java/com/jnu/recyclerview_demo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.jnu.recyclerview_demo;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
