fbbcef3c43e7808c7aa162d22cf7baf631d7d45d
XiaofengQiu <18320651165@163.com>
2023-10-20 17:30:04 +0800

    实验七: RecleView基础上实现ContextMenu,并实现添加Item功能

---
+++ Android_Demo/app/src/main/java/com/jnu/recyclerview_demo/BookItemDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.jnu.recyclerview_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookItemDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_item_details);
+    }
+}
