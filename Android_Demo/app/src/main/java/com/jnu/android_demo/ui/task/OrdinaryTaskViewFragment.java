280770a4cda458d81088ee8004f92e49bd928a50
XiaofengQiu <18320651165@163.com>
2023-12-06 09:54:07 +0800

    fix: 修复 点击“新建任务”后闪退 bug
    在Manifest.xml中添加AddTaskItemActivity的配置

--- Android_Demo/app/src/main/java/com/jnu/android_demo/ui/task/OrdinaryTaskViewFragment.java
+++ Android_Demo/app/src/main/java/com/jnu/android_demo/ui/task/OrdinaryTaskViewFragment.java
@@ -1,11 +1,12 @@
 package com.jnu.android_demo.ui.task;
 
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import androidx.fragment.app.Fragment;
 
 import com.jnu.android_demo.R;
 
